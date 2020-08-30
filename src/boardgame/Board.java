6063b66578b3301e602841634ac5baf191cc180e
Vagner-Oli <vaolifelix@gmail.com>
2020-08-30 17:39:19 -0300

    Moving Pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -37,11 +37,24 @@ public class Board {
 	}
 
 	public void placePiece(Piece piece, Position position) {
-		if(thereIsAPiece(position)) {
-			throw new BoardException("There's already a piece on position "+position);
+		if (thereIsAPiece(position)) {
+			throw new BoardException("There's already a piece on position " + position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 
 	private boolean positionExists(int row, int column) {
@@ -58,4 +71,5 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+
 }
